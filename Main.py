77ce8cf351c64a0b7e747789a4444c7d5a4c3aa4
Gugasujith <91770350+Gugasujith@users.noreply.github.com>
2022-07-20 11:29:01 +0530

    Ex 1

--- Main.py
+++ Main.py
@@ -1,33 +1,54 @@
 import os
 class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
+    def __init__(s,n):
+        s.n= n
+        s.st=[None]*n
+        s.top = -1
 
-    def is_empty(self):
-        # Write code here
+    def push(s):
+       if s.top == s.n-1:
+            print("Stack full!")
+       else:
+            s.top += 1
+            data = int(input("Data to be pushed: "))
+            s.st[s.top] = data
 
-    def is_full(self):
-        # Write code here
+    def pop(s):
+      if s.top == -1:
+            print("Empty stack")
+       else:
+            s.top = s.top-1
+            data = s.st[s.top]
+            return data
+     
+    def peek(s):
+       print(s.st[s.top])
+        
 
-    def push(self, data):
-        if not self.is_full():
-            # Write code here
+    def display(self):
+        for i in range(s.top + 1):
+            print(s.st[i],end=",")
 
-    def pop(self):
-        if not self.is_empty():
-            # Write code here
+n = int(input("Enter the size of integer: "))
+s1 = stackADT(n)
 
-    def status(self):
-        # Write code here
-
-# Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+while(1):
+    print("\n***SPECIFY BY THE FUNCTIONS BY NUMBERS***")
+    print("\n 1.Push \n 2.Pop \n 3.Peek \n 4.Display \n 5.EXIT")
+    ch = int(input("Function to be performed: "))
+    
+    if ch == 1:
+        print("Inserted")
+        s1.push()
+    elif ch == 2:
+        print("Deleted")
+        s1.pop()
+    elif ch == 3:
+        print("Top element dislay")
+        s1.peek()
+    elif ch==4:
+        ("Total list display")
+        s1.display()
+    else:
+        print("Thank you , have a nice day!")
+        break
